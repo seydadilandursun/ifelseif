ef606dd21eaf875890236cfa638d99d6ee79c704
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-07-23 16:43:18 +0300

    ifelseif yapildi

---
+++ ifelseif/Program.cs
@@ -0,0 +1,18 @@
+﻿int time=DateTime.Now.Hour;
+if(time>=6 && time<=11){
+    Console.WriteLine("Gunaydin");
+}
+else if (time<=18)
+{
+    Console.WriteLine("Iyi gunler");
+}
+else
+{
+    Console.WriteLine("iyi geceler");
+}
+
+string sonuc=time<=18 ? "iyi gunler" : "iyi geceler";
+
+
+sonuc = time>=6 && time<11 ? "gunaydin" : time<=18 ? "iyi gunler": "iyi geceler";
+Console.WriteLine(sonuc);
